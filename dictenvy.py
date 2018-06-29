2fc7455a58afae4e7a20b3e01414396e524e5b5c
Roman Tomjak <roman.tomjak@made.com>
2018-06-29 14:31:38 +0100

    fix simple value merging

--- dictenvy.py
+++ dictenvy.py
@@ -7,14 +7,20 @@ def merge(d1, d2):
     ret = d1.copy()
     for key in d2.keys():
         if key in ret:
-            if isinstance(ret[key], dict):
-                ret[key].update(merge(ret[key], d2[key]))
+            if isinstance(d2[key], dict) and isinstance(ret[key], dict):
+                ret[key] = merge(ret[key], d2[key])
+            elif isinstance(d2[key], dict):
+                ret[key] = merge({'': ret[key]}, d2[key])
+            elif isinstance(ret[key], dict):
+                ret[key] = merge(ret[key], {'': d2[key]})
+            else:
+                ret[key] = d2[key]
         else:
             ret[key] = d2[key]
     return ret
 
 
-def dict_from_key_path(segments, value):
+def __dict_from_key_path(segments, value):
     """Creates a nested dict out of array of keys."""
     ret = {}
     for segment in reversed(segments):
@@ -29,17 +35,30 @@ def dictate(store, depth):
     """Transforms environment variables into a dictionary.
 
     Key in conjugation with depth is what controls the depth of the nested
-    dicts.
+    dicts. Variables that start with an underscore will be left alone.
+
+    Sometimes you may encounter the following situation:
+
+        TERM_PROGRAM=Apple_Terminal
+        TERM=xterm-256color
+    
+    In which case, the return value would still be a dict, but `TERM` value
+    would have an empty key.
 
     Examples:
         store={'A_B_C': 1}, depth=0 => {'a_b_c': 1}}
         store={'A_B_C': 1}, depth=1 => {'a': {'b_c': 1}}
         store={'A_B_C': 1}, depth=2 => {'a': {'b': {'c': 1}}}
+
+        store={'A': 1, 'A_B': 2}, depth=1 => {'a': {'': '1', 'b': '2'}}
     """
     ret = {}
     for key, value in store.items():
-        # fixme: provide a mapping to dictate() instead of using .replace() on key
-        parts = key.lower().replace('__', '').split('_', depth)
-        dict_value = dict_from_key_path(parts, value)
+        lkey = str(key).lower()
+        if lkey.startswith('_'):
+            ret[lkey] = value
+            continue
+        parts = lkey.split('_', depth)
+        dict_value = __dict_from_key_path(parts, value)
         ret = merge(ret, dict_value)
     return ret
--- tests.py
+++ tests.py
@@ -16,6 +16,11 @@ def test_transforms_into_dict_one_level_with_one_underscore():
     assert dictenvy.dictate(store, depth=1) == {'a': {'b_hello': 1}}
 
 
+def test_transforms_into_dict_one_level_with_empty_key():
+    store = {'A': 1, 'A_B': 2}
+    assert dictenvy.dictate(store, depth=1) == {'a': {'': 1, 'b': 2}}
+
+
 def test_transforms_into_dict_two_levels():
     store = {'A_B_C': 1}
     assert dictenvy.dictate(store, depth=2) == {'a': {'b': {'c': 1}}}
@@ -29,3 +34,26 @@ def test_transforms_into_dict_two_levels_two_items_with_one_underscore():
 def test_transforms_into_dict_three_levels_with_two_items():
     store = {'A_B_C_HELLO': 1, 'A_B_C_WORLD': 2}
     assert dictenvy.dictate(store, depth=3) == {'a': {'b': {'c': {'hello': 1, 'world': 2}}}}
+
+
+def test_variables_starting_with_underscores_are_left_intact():
+    store = {'_A_B': 1}
+    assert dictenvy.dictate(store, depth=1) == {'_a_b': 1}
+
+
+def test_merge_simple():
+    d1 = {'a': 2}
+    d2 = {'a': 1}
+    assert dictenvy.merge(d1, d2) == {'a': 1}
+
+
+def test_merge_does_not_overwrite_existing_keys():
+    d1 = {'a': {'b': 1}}
+    d2 = {'a': {'c': 2}}
+    assert dictenvy.merge(d1, d2) == {'a': {'b': 1, 'c': 2}}
+
+
+def test_merge_creates_empty_key_for_simple_values():
+    d1 = {'a': 1}
+    d2 = {'a': {'b': 2}}
+    assert dictenvy.merge(d1, d2) == {'a': {'': 1, 'b': 2}}
